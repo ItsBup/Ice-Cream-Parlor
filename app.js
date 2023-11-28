bf4457db4b0732a36c78619d389156bcf65eedc5
Kaden Rigsbee <kjrigsbee@gmail.com>
2023-11-28 10:49:48 -0700

    Good Ice Cream

---
+++ app.js
@@ -0,0 +1,33 @@
+console.log('hello there! -Obi-Wan Kenobi')
+
+const iceCream = [{
+    name: 'Cookie Dough',
+    price: 1.25,
+    quantity: 0
+}, 
+{
+    name: 'Vanilla',
+    price: 1,
+    quantity: 0
+}, 
+{
+    name: 'Strawberry',
+    price: 1.25,
+    quantity: 0
+}]
+
+const toppings = [{
+    name: 'Sprinkles',
+    quantity: 0,
+    price: .25
+}, 
+{
+    name: 'Chocolate Chips',
+    price: .25,
+    quantity: 0
+},
+{
+    name: 'Cookie Chunks',
+    price: .5,
+    quantity: 0
+}]
